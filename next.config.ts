2f0b8603e1a14ba9e2436edf5d20d932adfc9d97
Natnael Tamiru <natnaeltamiru98@gmail.com>
2026-07-16 23:16:25 +0300

    Allow DiceBear profile avatars

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "primely-api.onrender.com",
       },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
     ],
   },
 };
